ecb9364bc4069bf7af6244ba7a958ca6b3b4e8ec
klebs6 <tpk3.mx@gmail.com>
2025-05-22 17:08:01 -0400

    add some comments

--- basic-android-integration/src/basic_android_integration.rs
+++ basic-android-integration/src/basic_android_integration.rs
@@ -3,9 +3,17 @@ crate::ix!();
 fn example_function() {
 
     let env:                *mut jni::sys::JNIEnv          = todo!();
+
+    // get this from the java side. it probably comes from your midi device selector
     let midi_device_obj:    jni::sys::jobject              = todo!();
+
+    // this will be initialize by the call below
     let out_device_ptr_ptr: *mut *mut ndk_sys::AMidiDevice = todo!();
 
+    // after this call, the initialized ndk AMidiDevice output device will be written through out_device_ptr_ptr
+    //
+    // you pass the JNIEnv into this call, as well as a jobject which represents the midiDevice you
+    // get from java
     let status:             ndk_sys::media_status_t        = unsafe { 
         ndk_sys::AMidiDevice_fromJava(
             env,
